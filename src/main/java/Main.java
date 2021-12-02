a0151677e0f971e01ac122250487fa8ea7eed503
Jan <Jan.Peltzer@iCloud.com>
2021-12-02 11:27:05 +0100

    Tut Test 11:27

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ public class Main {
         System.out.println("Ten = "+ getTen());
         System.out.println("mainNeuewf");
  //       dumm Fehler
-        // sdfsdg hgjjmghm fdgfdh
+        // sdfsdg hgjjmghm fdgfdh fdgf
 
     }
 }
