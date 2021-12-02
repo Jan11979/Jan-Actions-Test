eafba5aebf650a8b97e0a1d2674e554923379c9b
Jan <Jan.Peltzer@iCloud.com>
2021-12-02 10:31:11 +0100

    e ignored, and an empty message aborts the commit.

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static int getTen() {
+    return 10;
+    }
+
+    public static void main(String[] args) {
+
+
+        System.out.println("main");
+        System.out.println("Ten = "+ getTen());
+
+
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+
+    @Test
+    void myGetTenTest() {
+        assertTrue(Main.getTen() == 10 );
+
+    }
+
+}
